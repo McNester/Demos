c2cf43e41b36761058e75244315f5ecefa18b1df
McNester <seraf5@bk.ru>
2024-05-03 04:32:22 +0500

    resolved the package error

--- src/main.js
+++ src/main.js
@@ -1,22 +1,12 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { VueShowdownPlugin } from 'vue-showdown';
 import App from './App.vue'
 import components from '@/components'
 
 import routes from '@/pages';
 
 const app = createApp(App)
-
-app.use(VueShowdownPlugin, {
-  // set default flavor of showdown
-  flavor: 'github',
-  // set default options of showdown (will override the flavor options)
-  options: {
-    emoji: false,
-  },
-});
 
 components.forEach((component) => {
   app.component(component.name, component)
